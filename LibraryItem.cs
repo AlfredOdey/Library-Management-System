3d5945edc0ce589637393f8868e1ae9e4389c3fb
Alfred Odey <alfredmodey@gmail.com>
2024-07-22 21:31:24 +0100

    First commit of Library Management files

---
+++ LibraryItem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public abstract class LibraryItem
+    {
+        public abstract string Title { get; set; }
+        public abstract string Author { get; set; }
+        public abstract void GetInfo();
+
+    }
+
+    public class Book : LibraryItem
+    {
+        public override string Title { get; set; }
+        public override string Author { get; set; }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Title: Far From The End\nAuthor: Lilian Chidiogo Ezejelue");
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        private string Email { get; set; }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("Name:\nEmail:");
+        }
+
+        public class Member : Person
+        {
+            private string name;
+            private string email;
+
+            public override void DisplayInfo()
+            {
+                Console.WriteLine("Name: Alfred\nEmail: alfredmodey@gmail.com");
+            }
+        }
+    }
+    
+}
