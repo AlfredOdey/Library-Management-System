94c283d2547e7957ff6ce924d8d90e2b3924cf79
Alfred Odey <alfredmodey@gmail.com>
2024-08-04 13:26:10 +0100

    A commit of Program.cs

--- Program.cs
+++ Program.cs
@@ -13,30 +13,27 @@ namespace LibraryManagementSystem
             Library library = new Library();
 
             Member member1 = new Member(1, "Alfred", "alfredmodey@gmail.com");
-            Member member2 = new Member(2, "Ese", "odeyalfred0@gmail.com");
 
             library.RegisterMember(member1);
-            library.RegisterMember(member2);
 
-            Book book1 = new Book("Far From The End", "Lilian Chidiogo Ezejelue", "1234");
-            Book book2 = new Book("Purple Hibiscus", "Chimamanda Ngozi Adichie", "5678");
+            Book book1 = new Book("Far From The End", "Lilian Chidiogo Ezejelue", "9789789744220");
+            Book book2 = new Book("The Martian", "Andy Weir", "9780804139021");
 
             library.AddBook(book1);
             library.AddBook(book2);
 
-            library.BorrowBook(1, 1, "1234");
-            library.BorrowBook(2, 2, "5678");
+            library.BorrowBook(1, 1, "9789789744220");
+            library.BorrowBook(2, 1, "9780804139021");
 
-            Console.WriteLine("All books...");
+            Console.WriteLine("AVAILABLE BOOKS");
             library.DisplayAllbooks();
 
-            Console.WriteLine("All members...");
+            Console.WriteLine("REGISTERED MEMBERS");
             library.DisplayAllMembers();
 
-            Console.WriteLine("All borrow transactions...");
+            Console.WriteLine("BORROW TRANSACTIONS");
             library.DisplayAllTransactions();
 
-            Console.WriteLine();
 
         }
     }
